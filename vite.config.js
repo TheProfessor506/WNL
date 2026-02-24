bb6eec24fdd20c3ce22af3a4b34b3db427e22453
TheProfessor506 <matei506gaming@gmail.com>
2026-02-24 10:25:30 +0200

    hopeful fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // Ensures assets are loaded relative to the WNL repository on Github Pages
+  base: '/WNL/', // Ensures assets are loaded relative to the WNL repository on Github Pages
   plugins: [react()],
 })
